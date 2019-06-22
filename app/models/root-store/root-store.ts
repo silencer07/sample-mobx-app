58e41df92c048fe5840f179fe95ec5ca1287b2a6
Aldrin Tingson <aldrintingson@gmail.com>
2019-06-22 17:52:07 +0800

    integrated todo store

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,11 +1,14 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { NavigationStoreModel } from "../../navigation/navigation-store"
+import { ToDoListStoreModel } from "../to-do-list-store/to-do-list-store"
+
 
 /**
  * An RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
   navigationStore: types.optional(NavigationStoreModel, {}),
+  toDoStore: types.optional(ToDoListStoreModel, {}),
 })
 
 /**
