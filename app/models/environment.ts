ffee3e6ef5f33e1ba8d8bc93df0f15a4a1f25c25
Aldrin Tingson <aldrintingson@gmail.com>
2019-06-22 17:11:26 +0800

    copy-pasted todo-api

--- app/models/environment.ts
+++ app/models/environment.ts
@@ -1,11 +1,27 @@
 import { Reactotron } from "../services/reactotron"
 import { Api } from "../services/api"
+import { ToDoApi } from "../services/todos-api"
 
 /**
  * The environment is a place where services and shared dependencies between
  * models live.  They are made available to every model via dependency injection.
  */
 export class Environment {
+
+  constructor() {
+    // create each service
+    this.reactotron = new Reactotron()
+    this.api = new Api()
+    this.todoApi = new ToDoApi()
+  }
+
+  async setup() {
+    // allow each service to setup
+    await this.reactotron.setup()
+    await this.api.setup()
+    await this.todoApi.setup()
+  }
+
   /**
    * Reactotron is only available in dev.
    */
@@ -15,4 +31,9 @@ export class Environment {
    * Our api.
    */
   api: Api
+
+  /**
+   * Example To Do List Api
+   */
+  todoApi: ToDoApi
 }
